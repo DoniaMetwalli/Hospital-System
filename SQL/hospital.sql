5605c07afe29aa44bfc871eede481a184561b514
y.alsayed <y.alsayed@nu.edu.eg>
2023-05-14 01:48:26 +0300

    some edits in erd + initial sql file

---
+++ SQL/hospital.sql
@@ -0,0 +1,77 @@
+CREATE DATABASE hemodialysisApp;
+USE hemodialysisApp;
+DROP TABLE hospital;
+
+CREATE TABLE hospital
+(
+    hospital_id  BIGINT UNSIGNED unique primary key not null,
+    email        varchar(255),
+    phone_number varchar(255),
+    city         varchar(255),
+    state        varchar(255),
+    address      varchar(255)
+);
+
+insert into hospital(hospital_id)
+values (uuid_short());
+
+CREATE TABLE user
+(
+    user_id      BIGINT UNSIGNED unique primary key not null,
+    first_name   varchar(255),
+    last_name    varchar(255),
+    phone_number varchar(255),
+    email        varchar(255),
+    user_type    char
+);
+
+CREATE table patient
+(
+    birthday   DATE,
+    gender     char,
+    patient_id bigint unsigned unique not null,
+    foreign key (patient_id) references user (user_id),
+    primary key (patient_id)
+);
+
+CREATE table doctor
+(
+    birthday    DATE,
+    gender      char,
+    doctor_id   bigint unsigned unique not null,
+    hospital_id bigint unsigned unique not null,
+    foreign key (hospital_id) references hospital (hospital_id),
+    foreign key (doctor_id) references user (user_id),
+    primary key (doctor_id)
+);
+
+CREATE table dialysis_machine
+(
+    price               int,
+    availability        bool,
+    dialysis_machine_id bigint unsigned unique not null primary key,
+    hospital_id         bigint unsigned unique not null,
+    foreign key (hospital_id) references hospital (hospital_id)
+);
+
+CREATE table medical_record
+(
+    doctor_diagnose varchar(255),
+    record_id       bigint unsigned unique not null primary key,
+    appointment_id  bigint unsigned unique not null,
+    foreign key (appointment_id) references appointment(appointment_id)
+);
+
+CREATE table appointment
+(
+    status varchar(255),
+    time DATETIME,
+    appointment_id  bigint unsigned unique not null primary key ,
+    record_id       bigint unsigned unique not null primary key,
+    dialysis_machine_id       bigint unsigned unique not null ,
+    hospital_id       bigint unsigned unique not null ,
+    doctor_id       bigint unsigned unique not null ,
+    foreign key (dialysis_machine_id) references dialysis_machine(dialysis_machine_id),
+    foreign key (hospital_id) references hospital(hospital_id),
+    foreign key (doctor_id) references doctor(doctor_id)
+);
