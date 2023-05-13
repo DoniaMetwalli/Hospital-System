028bc2c20a45ef8c3ddbfa2606339b1be5f6dc86
Donia Metwalli <doniametwallixx@gmail.com>
2023-05-13 21:09:19 +0300

    Added the home page

--- Frontend/lib/pages/home_page.dart
+++ Frontend/lib/pages/home_page.dart
@@ -8,10 +8,45 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  get tapCallback => null;
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Home Page"),
+    
+    Future openDetails() => showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text("DEETS"),
+        actions: [
+          TextButton(onPressed: close , child: const Text("Okay / Done"))
+        ],
+      ));
+
+    return Scaffold(
+      appBar: AppBar
+      (
+        title: const Text("Upcoming Appointments"),
+        backgroundColor: const Color.fromARGB(255, 192, 214, 192),
+      ),
+      backgroundColor: const Color.fromARGB(255, 192, 214, 192),
+      body: ListView(children: [
+        Card(
+          color: const Color.fromARGB(255, 125, 206, 167),
+          child: ListTile(
+            title: const Text("Hospital Name", style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500,),), 
+            subtitle: const Text("Doctor's Name \nDate", style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400,)),
+            isThreeLine: true,
+            onTap: () {openDetails();},
+        ),
+        
+        )
+      ]),
     );
+
+  }
+
+  void close() 
+  {
+    Navigator.of(context).pop();
   }
 }
