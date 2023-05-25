311ef223e6dfebdbb598b7e084f61325883ff42b
y.alsayed <y.alsayed@nu.edu.eg>
2023-05-26 00:09:58 +0300

    fix time in up appoiment card

--- Frontend/lib/components/home_page/upcoming_appoinment_card.dart
+++ Frontend/lib/components/home_page/upcoming_appoinment_card.dart
@@ -1,10 +1,9 @@
-
 import 'package:flutter/material.dart';
 import 'package:hemodialysis_csci305/backend/api_connection.dart';
 
 import '../../backend/shared_variables.dart';
 
-Card upcomingAppoinmentCard(Map<String, dynamic> appointmentsList,BuildContext context) {
+Card upcomingAppoinmentCard(Map<String, dynamic> appointmentsList, BuildContext context) {
   return Card(
     color: cardColor,
     margin: const EdgeInsets.only(left: 16, right: 16, top: 16, bottom: 8),
@@ -17,45 +16,46 @@ Card upcomingAppoinmentCard(Map<String, dynamic> appointmentsList,BuildContext c
           fontWeight: FontWeight.w600,
         ),
       ),
-      subtitle: Text("Hospital Address: ${appointmentsList["hospital"]["address"]}\n Appointment Time: ${appointmentsList["appointment"]["time"]}",
+      subtitle: Text(
+          "Hospital Address: ${appointmentsList["hospital"]["address"]}\n Appointment Time: ${appointmentsList["appointment"]["time"]}",
           style: const TextStyle(
             // fontSize: 8,
             fontWeight: FontWeight.w400,
           )),
       isThreeLine: true,
-      onTap: () => upcomingAppoinmentAlert(appointmentsList,context),
+      onTap: () => upcomingAppoinmentAlert(appointmentsList, context),
     ),
   );
 }
 
-Future<dynamic> upcomingAppoinmentAlert(Map<String, dynamic> appointmentsList,BuildContext context) {
+Future<dynamic> upcomingAppoinmentAlert(
+    Map<String, dynamic> appointmentsList, BuildContext context) {
   final hospital = appointmentsList["hospital"];
   final appointment = appointmentsList["appointment"];
   return showDialog(
     context: context,
     builder: (context) => AlertDialog(
       title: const Text("Details"),
-      content: SelectableText("Hospital Name: ${hospital["name"]}\nHospital Address: ${hospital["address"]}\nAppointment Time: ${appointment["time"]}\nDoctor's Name: ${appointmentsList["doctorName"]}\nHospital Phone: ${hospital["phone_number"]},\nDoctor's Phone: ${appointmentsList["doctorPhone"]},\nHospital E-mail: ${hospital["email"]}"),
+      content: SelectableText(
+          "Hospital Name: ${hospital["name"]}\nHospital Address: ${hospital["address"]}\nAppointment Time: ${appointment["time"]}\nDoctor's Name: ${appointmentsList["doctorName"]}\nHospital Phone: ${hospital["phone_number"]},\nDoctor's Phone: ${appointmentsList["doctorPhone"]},\nHospital E-mail: ${hospital["email"]}"),
       actions: [
         TextButton(
           onPressed: () => changeAppointment(
-          appointmentId: appointment["appointment_id"],
-          patientId: box.get("userId"),
-          dialysisMachineId: appointment["dialysis_machine_id"],
-          doctorId: appointment["doctor_id"],
-          hospitalId: appointment["hospital_id"],
-          status: "canceled",
-          time: "2023-05-15 00:00:00",
-          slot: 0,
+            appointmentId: appointment["appointment_id"],
+            patientId: box.get("userId"),
+            dialysisMachineId: appointment["dialysis_machine_id"],
+            doctorId: appointment["doctor_id"],
+            hospitalId: appointment["hospital_id"],
+            status: "canceled",
+            time: appointment["time"],
+            slot: 0,
+          ),
+          child: const Text("Cancel Appointment"),
         ),
-  child: const Text("Cancel Appointment"),
-),
-
-      TextButton(
-        onPressed: () => close(context),
-        child: const Text("Okay"),
-      ),
-        
+        TextButton(
+          onPressed: () => close(context),
+          child: const Text("Okay"),
+        ),
       ],
     ),
   );
